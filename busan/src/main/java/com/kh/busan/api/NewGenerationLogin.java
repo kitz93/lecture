2d8fe58ae79b81b7c3a711736213c482fab45bec
kitz93 <star0030@naver.com>
2025-01-22 17:06:09 +0900

    0122 last

---
+++ busan/src/main/java/com/kh/busan/api/NewGenerationLogin.java
@@ -0,0 +1,14 @@
+package com.kh.busan.api;
+
+public class NewGenerationLogin {
+	
+	// 요청 보낼 때 요청 URL에 뭘 요청하는지 보내기
+	
+	// 행위로 메소드를 구분
+		// GET - 조회
+		// POST - 생선
+		// PUT - 수정
+		// DELETE - 삭제
+	
+
+}
--- busan/src/main/java/com/kh/busan/api/model/vo/CommentDTO.java
+++ busan/src/main/java/com/kh/busan/api/model/vo/CommentDTO.java
@@ -1,5 +1,8 @@
 package com.kh.busan.api.model.vo;
 
+import java.io.Serializable;
+
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,9 +16,10 @@ import lombok.ToString;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class CommentDTO {
+public class CommentDTO implements Serializable {
 	
 	private Long foodNo;
+	@NotBlank(message="작성자는 필수")
 	private String writer;
 	private String content;
 
